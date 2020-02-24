292bd2a7fb8e86768a434c1a52d198cdb1a6c5d9
Pavel <pavelp44@yandex.ru>
2020-02-24 19:00:58 +0300

    Тест 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,6 +2,12 @@ import org.junit.Test;
 
 class MainClass {
 
+    private int class_number = 20;
+
+    public int getClassNumber(){
+    return class_number;
+    }
+
     public int getLocalNumber(){
         return 14;
     }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,7 +5,12 @@ import sun.applet.Main;
 public class MainClassTest extends MainClass {
 
     @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("class_number <= 45", getClassNumber() > 45);
 
+    }
+
+    @Test
     public void testGetLocalNumber(){
         Assert.assertTrue("Number is not equal 14",getLocalNumber() == 14);
     }
